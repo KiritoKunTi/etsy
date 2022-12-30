4cb8e9524372db03e1ea5b7c1a1c25cd3d7dda0a
Zanggar Zhuamagaliyev <200103287@stu.sdu.edu.kz>
2022-12-30 20:38:54 +0600

    functions has been added to product and parameters tables

--- backend/db/product.go
+++ backend/db/product.go
@@ -1,1 +1,54 @@
 package db
+
+type Product struct {
+	ID                int                `json:"id"`
+	UserID            int                `json:"user_id"`
+	Name              string             `json:"name"`
+	Price             int                `json:"price"`
+	Amount            int                `json:"amount"`
+	Description       string             `json:"description"`
+	ProductParameters []ProductParameter `json:"product_parameters"`
+}
+
+func (product *Product) Create() (err error) {
+	st, err := DB.Prepare("INSERT INTO PRODUCTS(USER_ID, NAME, PRICE, AMOUNT, DESCRIPTION) VALUES ($1, $2, $3, $4, $5) RETURNING ID")
+	if err != nil {
+		return
+	}
+	defer st.Close()
+	err = st.QueryRow(
+		product.UserID, product.Name, product.Price, product.Amount, product.Description,
+	).Scan(&product.ID)
+	if err != nil {
+		return
+	}
+	err = product.CreateParameters()
+	if err != nil {
+		product.Delete()
+		return
+	}
+	return
+}
+func (product *Product) CreateParameters() (err error) {
+	for _, parameter := range product.ProductParameters {
+		err = parameter.Create(product.ID)
+		return
+	}
+	return
+}
+
+func (product *Product) Delete() (err error) {
+	stmt, err := DB.Prepare("DELETE FROM PRODUCT_PARAMETERS WHERE PRODUCT_ID=$1")
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID)
+	stmt, err = DB.Prepare("DELETE FROM PRODUCTS WHERE ID=$1")
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID)
+	return
+}
--- backend/db/product_parameters.go
+++ backend/db/product_parameters.go
@@ -1,1 +1,18 @@
 package db
+
+type ProductParameter struct {
+	ID        int    `json:"id"`
+	ProductID int    `json:"product_id"`
+	Key       string `json:"key"`
+	Value     string `json:"value"`
+}
+
+func (parameter *ProductParameter) Create(product_id int) (err error) {
+	stmt, err := DB.Prepare("INSERT INTO PRODUCT_PARAMETERS(PRODUCT_ID, KEY, VALUE) VALUES ($1, $2, $3) RETURNING ID")
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(product_id, parameter.Key, parameter.Value).Scan(&parameter.ID)
+	return
+}
